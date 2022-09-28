4252af39a8a77f2f91827351bd8bf4ff54736964
jyoth <jyothsna.kalpam@gmail.com>
2022-09-28 12:23:17 +0530

    CommitOne

---
+++ GradleOne/src/test/NewTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	WebDriver driver;
+	@Test
+	public void basic() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	}
+}
